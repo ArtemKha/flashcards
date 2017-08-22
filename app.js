e2cb1e94003a24565ce697ecfad539a50bbf9705
artemkha <khaapt@gmail.com>
2017-08-22 12:17:14 +0300

    pug structure

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require('express');
 
 const app = express()
 
+
+const colors = [
+  'Red', 
+  'Green', 
+  'Blue', 
+  'Hue', 
+  'Saturation',
+  'purple'
+]
+
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/card', (req, res) => {
+app.get('/cards', (req, res) => {
   res.locals.prompt = 'Who is buried in Grant\'s tomb?'
+  res.locals.hint = 'Think about whose tomb it is.'
+  res.locals.colors = colors
   res.render('card')
 })
 
