218d6dfce6666b73407cdc060e623a3c221d32dc
artemkha <khaapt@gmail.com>
2017-08-22 13:15:28 +0300

    cookie

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
 app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
@@ -10,10 +13,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-  res.render('hello')
+  res.render('hello', { name: req.cookies.username })
 })
 
 app.post('/hello', (req, res) => {
+  res.cookie('username', req.body.username )
   res.render('hello', { name: req.body.username })
 })
 
