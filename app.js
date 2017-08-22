daf856ec63c58091ba9d3ecefab021711fa8e6c6
artemkha <khaapt@gmail.com>
2017-08-22 11:30:11 +0300

    added pug

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Try hero!')
 })
+app.set('view engine', 'pug')
 
-app.listen(3000)
+app.get('/hello', (req, res) => {
+  res.send('Hello, hero!')
+})
+
+app.listen(3000, () => {
+  console.log('The app is running on localhost:3000.')
+})
