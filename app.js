b87d56e1a0c41eb4ade6d9dc3ca67f6cef8dc86a
artemkha <khaapt@gmail.com>
2017-08-22 17:27:49 +0300

    errors handler

--- app.js
+++ app.js
@@ -39,6 +39,16 @@ app.get('/cards', (req, res) => {
   res.render('card')
 })
 
+app.use( (req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+app.use( (err, req, res, next) => {
+  res.locals.error = err
+  res.status(err.status)
+  res.render('error')
+})
 app.listen(3000, () => {
   console.log('The app is running on localhost:3000.')
 })
