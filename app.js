2788255096438128ce75b9da3780abf4238b8957
artemkha <khaapt@gmail.com>
2017-08-22 17:46:32 +0300

    added route files

--- app.js
+++ app.js
@@ -8,36 +8,11 @@ app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  const name = req.cookies.username
-  name ? res.render('index', { name }) 
-       : res.redirect('/hello')
-})
+const mainRoutes = require('./routes')
+const cardRoutes = require('./routes/cards')
 
-app.get('/hello', (req, res) => {
-  const name = req.cookies.username
-  name ? res.redirect('/')
-       : res.render('hello') 
-})
-
-app.post('/', (req, res) => {
-  res.clearCookie('username')
-  res.redirect('/hello')
-})
-
-app.post('/hello', (req, res) => {
-  res.cookie('username', req.body.username )
-  res.redirect('/')
-})
-
-app.get('/cards', (req, res) => {
-  res.locals = {
-    prompt: 'Who is buried in Grant\'s tomb?',
-    hint: 'Think about whose tomb it is.',
-    colors
-  }
-  res.render('card')
-})
+app.use(mainRoutes)
+app.use('/cards', cardRoutes)
 
 app.use( (req, res, next) => {
   const err = new Error('Not Found')
---
+++ routes/cards.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.locals = {
+    prompt: 'Who is buried in Grant\'s tomb?',
+    hint: 'Think about whose tomb it is.',
+  }
+  res.render('card')
+})
+
+module.exports = router
